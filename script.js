8f88a3a3771d8c3ba33d811f63b3ea4d5f8bc95a
Mark Johnston <mjstone587@Yahoo.com>
2022-01-26 13:03:38 -0600

    added working images urls to json data and moved some data around. Created function to grab information from JSON file and appropriately populate container 2. Made some minor style changes to improve look.

--- script.js
+++ script.js
@@ -10,12 +10,30 @@ const btn = document.querySelectorAll(".btn");
 const modal = document.querySelector(".modal");
 const posters = document.querySelectorAll(".individual_posters");
 
-// functions for opening and closing all modals and overlays
+/* Generate Random Number based on length of api array length */
+const randomNum = function (data) {
+  return Math.floor(Math.random() * data.movies.length);
+};
+
+/* Populate Container 2 with data from API */
+const populateCont2 = function (data) {
+  for (var i = 0; i < 4; i++) {
+    const posterImg = document.querySelectorAll(".poster_img");
+    const h1 = document.querySelectorAll(".poster_h1");
+    const h2 = document.querySelectorAll(".poster_h2");
+    const h3 = document.querySelectorAll(".poster_h3");
+    posterImg[i].src = data.movies[i].poster;
+    h1[i].innerHTML = data.movies[i].title;
+    h2[i].innerHTML = data.movies[i].director;
+    h3[i].innerHTML = data.movies[i].released;
+  }
+};
+// functions for opening modal
 const openModal = function () {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
-
+/* function to close modal */
 const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
@@ -28,17 +46,24 @@ for (let i = 0; i < btn.length; i++) {
   });
 }
 
-/* When clicking close button close modals and overlays */
-btnClose.addEventListener("click", () => {
-  closeModal();
+/* Close modal when clicking overlay or close button */
+window.addEventListener("click", function (event) {
+  if (event.target == overlay || event.target == btnClose) {
+    closeModal();
+  }
 });
+const modalMoreInfo = (data) => {
+  console.log(btn.id);
+};
 
-const populateCont2 = function () {
+/* Fetch Data from API */
+const fetchData = function () {
   fetch("movieDB.json")
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      for (var i = 0; i < 4; i++) {}
+      populateCont2(data);
+      let getNum = randomNum(data);
     });
 };
-populateCont2();
+fetchData();
